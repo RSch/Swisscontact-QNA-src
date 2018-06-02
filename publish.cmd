d12498e5b068ffdfb2162cad3b8b3ded9375084f
Richard Schweizer <u10085@private.swisscontact.org>
2018-06-02 23:12:52 +0200

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=swisscontact-qna-b40b-Web-Deploy.pubxml -p:Password=GkDkKmnsfjdwtS3y52zJ2CN8oKejz87l73htkDG3EEkPgtDM6zz5fttao5jr
+
